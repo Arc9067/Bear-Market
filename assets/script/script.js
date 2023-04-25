212c5f25a8a259093461d08e1fe92de161f5b2c0
Mikail Rokib <atandarokib@gmail.com>
2023-04-25 20:50:55 +0100

    updated' '

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,34 @@ const openBtn = document.querySelector("#button");
 openBtn.addEventListener("click", () => {
   loadd.classList.add("none");
 });
+
+ScrollReveal({
+  reset: true,
+  duration: 2000,
+}).reveal(".header-txt");
+
+ScrollReveal({
+  reset: true,
+  duration: 1000,
+}).reveal(".hero-para");
+
+ScrollReveal({
+  reset: true,
+  duration: 1500,
+}).reveal(".button");
+
+ScrollReveal({
+  reset: true,
+  duration: 2500,
+  interval: 200,
+}).reveal(".logo");
+
+ScrollReveal({
+  reset: true,
+  duration: 2500,
+}).reveal(".abt-txt");
+
+ScrollReveal({
+  reset: true,
+  interval: 200,
+}).reveal(".abo-d");
